5353bdde6ebd01291b6b6ae4b5004a2513207400
Lee Machin <leemachin@users.noreply.github.com>
2014-02-01 12:18:23 +0000

    Calculate heights more intelligently, and lazily

--- jquery.sticky.js
+++ jquery.sticky.js
@@ -1,36 +1,35 @@
 (function ($) {
-  $.fn.sticky = function (opts) {
-    var elem = $(this),
-        nextVisible = elem.siblings().first(':visible'),
-        stuck = false,
-        height = 0
-        
-    // http://davidwalsh.name/ways-css-javascript-interact
-    var addCSSRule = function (sheet, selector, rules, index) {
-      if (sheet.insertRule) {
-        sheet.insertRule(selector + '{ ' + rules + ' }', index)
-      } else {
-        sheet.addRule(selector, rules, index)
-      }
-    }
-    
-    addCSSRule(document.styleSheets[0], '.stuck', 'position: fixed; top: 0; z-index: 5')
-    addCSSRule(document.styleSheets[0], '.stuck-offset', 'margin-top: ' + elem.outerHeight())
   
-    if (opts.offsetFrom) {
-      // offsetFrom can contain multiple elements, so get the total outerHeight
-      opts.offsetFrom.each(function (_, offsetElem) {
-        var elem = $(offsetElem)
-        if (elem.is(':visible')) {
-          height += $(offsetElem).outerHeight()
-        }
-      })
+  // http://davidwalsh.name/ways-css-javascript-interact
+  function addCSSRule (sheet, selector, rules, index) {
+   if (sheet.insertRule) {
+      sheet.insertRule(selector + '{ ' + rules + ' }', index)
     } else {
-      height = elem.outerHeight()
-    }
+      sheet.addRule(selector, rules, index)
+    }   
+  }
+  
+  function heightOffset (elem, defaultHeight) {
+    var height = 0
+    
+    elem.prevAll(':visible').each(function (_, sibling) {
+      height += $(sibling).outerHeight()
+    })
+    
+    return height || defaultHeight
+  }
+  
+  $.fn.sticky = function () {
+    var elem = $(this),
+        stuck = false,
+        defaultHeight = elem.outerHeight(),
+        styleSheet = document.styleSheets[0]
+        
+    addCSSRule(styleSheet, '.stuck', 'position: fixed; top: 0; z-index: 10')
+    addCSSRule(styleSheet, '.stuck-offset', 'margin-top: ' + elem.outerHeight())
   
     $(window).on('scroll', function () {
-      if ($(this).scrollTop() > height) {
+      if ($(this).scrollTop() > heightOffset(elem, defaultHeight)) {
         if (!stuck) {
           elem.addClass('stuck')
           nextVisible.addClass('stuck-offset')
