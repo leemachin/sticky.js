98164e76a6788ca96be3af30f9dae87c8137a8b3
Lee Machin <leemachin@users.noreply.github.com>
2014-02-02 18:49:44 +0000

    Flesh out previous ideas into something that actually works

--- jquery.sticky.js
+++ jquery.sticky.js
@@ -1,36 +1,45 @@
 (function ($) {
-  
+
   // http://davidwalsh.name/ways-css-javascript-interact
   function addCSSRule (sheet, selector, rules, index) {
    if (sheet.insertRule) {
       sheet.insertRule(selector + '{ ' + rules + ' }', index)
     } else {
       sheet.addRule(selector, rules, index)
-    }   
+    }
   }
-  
-  function heightOffset (elem) {
-    return elem.offset().top || elem.outerHeight()
-  }
-  
+
   $.fn.sticky = function () {
     var elem = $(this),
+        initialOffset = elem.offset().top,
         nextVisible = elem.siblings().first(':visible'),
         stuck = false,
         styleSheet = document.styleSheets[0]
-        
+
     addCSSRule(styleSheet, '.stuck', 'position: fixed !important; top: 0 !important; z-index: 10')
-  
+
     $(window).on('scroll', function () {
-      if ($(this).scrollTop() > heightOffset(elem)) {
-        
+      var currentPosition = $(window).scrollTop(),
+          offset = elem.offset().top
+
+      if (currentPosition < initialOffset && offset !== initialOffset && !stuck) {
+
+        // Update the initialOffset, because the DOM has caused the element's position to change
+        initialOffset = offset
+
+      } else if (currentPosition >= Math.max(offset, initialOffset)) {
+
         if (stuck) return;
-        
+
+        // The page has scrolled past the top position of the element, so fix it and
+        // apply its height as a margin to the next visible element so it doesn't jump
         elem.addClass('stuck')
         nextVisible.css('margin-top', elem.outerHeight())
         stuck = true
-        
+
       } else {
+
+        // Unstick, because the element can now rest in its original position
         elem.removeClass('stuck')
         nextVisible.css('margin-top', "")
         stuck = false
